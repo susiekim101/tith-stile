7ca33a16ded5017e680f8b2320a4f32a194e230c
Lavender Hwang <lhwang914@gmail.com>
2025-07-24 09:17:18 -0700

    Finish landing page (first-draft)

---
+++ src/pages/Landing/FooterSection.jsx
@@ -0,0 +1,23 @@
+import styles from "./FooterSection.module.css";
+import GetStartedButton from "./GetStartedButton";
+
+const FooterSection = () => {
+  return (
+    <section className={styles.footer}>
+      <h1>Ready to Transform Your Space?</h1>
+      <p className={styles.p}>
+        At Tori in the House, we do our best to practice ethical storytelling
+        and honor your dignity. We will never use your real name or any
+        identifying details without your permission.{" "}
+      </p>
+      <p className={styles.p}>
+        If there’s a name or nickname you’d like us to use in any future
+        storytelling (such as in photos, videos, or captions), feel free to
+        share it in the quiz.
+      </p>
+      <GetStartedButton textColor="text-light" bgColor="bgMaroon" />
+    </section>
+  );
+};
+
+export default FooterSection;
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,8 +1,9 @@
-import "./Landing.css";
+import styles from "./Landing.module.css";
 import HeroSection from "./HeroSection";
 import OverviewSection from "./OverviewSection";
 import SupportSection from "./SupportSection";
 import StartSection from "./StartSection";
+import FooterSection from "./FooterSection";
 
 /*
  * Section - Tagline Mapping
@@ -10,15 +11,17 @@ import StartSection from "./StartSection";
  * Overview - What to Expect
  * Mission - Support our Mission
  * Start - Let's Get Started
+ * Footer - Footer
  */
 
 function Landing() {
   return (
-    <div className="landingWrapper">
+    <div>
       <HeroSection />
       <OverviewSection />
       <SupportSection />
       <StartSection />
+      <FooterSection />
     </div>
   );
 }
