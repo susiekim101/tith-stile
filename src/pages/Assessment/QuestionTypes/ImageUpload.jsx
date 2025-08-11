89e9c0d38dd48844b1d113dd41fd168f898e222d
Susie Kim <35243112+susiekim101@users.noreply.github.com>
2025-08-11 09:31:34 +0900

    change image upload ui

--- src/pages/Assessment/QuestionTypes/ImageUpload.jsx
+++ src/pages/Assessment/QuestionTypes/ImageUpload.jsx
@@ -22,6 +22,7 @@ const ImageUpload = ( {formValues, setFormValues, id } ) => {
     if(!formValues[id]) {
         formValues[id] = [];
     }
+
     const deleteImages = async () => {
         const folderRef = ref(storage, `user-images/${userId}`);
 
@@ -29,6 +30,9 @@ const ImageUpload = ( {formValues, setFormValues, id } ) => {
             const images = await listAll(folderRef);
             const deletePromises = images.items.map((itemRef) => deleteObject(itemRef)); 
             await Promise.all(deletePromises);
+            console.log("selectedImages.length: ", selectedImages.length);
+            setSelectedImages([]);
+            formValues[id] = [];
             console.log("All images deleted for user");
         } catch (error) {
             console.error("Error deleting user images: ", error);
@@ -54,6 +58,7 @@ const ImageUpload = ( {formValues, setFormValues, id } ) => {
         try {
             const urls = await Promise.all(uploadPromise);
             setFormValues((prev) => ({...prev, [id]: urls}));
+            setSelectedImages(imageList);
             console.log(formValues[id]);
         } catch (error) {
             console.error("Upload failed: ", error);
@@ -63,7 +68,6 @@ const ImageUpload = ( {formValues, setFormValues, id } ) => {
         if (!images || images.length === 0) 
             return;
         const trimmedImages = Array.from(images).slice(0, MAX_FILES);
-        setSelectedImages(trimmedImages);
         handleImageUpload(trimmedImages); 
     };
 
@@ -94,34 +98,48 @@ const ImageUpload = ( {formValues, setFormValues, id } ) => {
     };
 
     return (
-        <>
-        <div
-            onDragOver={handleDragOver}
-            onDragLeave={handleDragLeave}
-            onDrop={handleDrop}
-            className={`${styles.uploadContainer} ${isDragging ? "styles.dragging" : ""}`}
-        >
-            <input 
-                type="file"
-                accept="image/*"
-                multiple
-                onChange={handleImageInputChange}
-                className={styles.hiddenInput}
-                id="image-upload"
-            />
-            <label 
-                htmlFor="image-upload"
-                className={styles.uploadButton}
+        <div className={styles.answerContainer}>
+            <div
+                onDragOver={handleDragOver}
+                onDragLeave={handleDragLeave}
+                onDrop={handleDrop}
+                className={`${styles.uploadContainer} ${isDragging ? styles.dragging : ""}`}
             >
-                <div>{(selectedImages.length == 0 && formValues[id].length == 0) ? <img src={upload} className={styles.uploadIcon}/> : <img src={uploadcomplete} className={styles.uploadIcon}/>}</div>
-                <div className={styles.uploadText}>Upload Images</div>
-            </label>
+                <input 
+                    type="file"
+                    accept="image/*"
+                    multiple
+                    onChange={handleImageInputChange}
+                    className={styles.hiddenInput}
+                    id="image-upload"
+                />
 
-            {console.log("Selected Images Length: ", selectedImages.length)}
-            {console.log("Form Values Length: ", formValues[id].length)}
+                <label htmlFor="image-upload"className={styles.uploadButton}>
+                    <div>{(selectedImages.length == 0 && formValues[id].length == 0) ? <img src={upload} className={styles.uploadIcon}/> : <img src={uploadcomplete} className={styles.uploadIcon}/>}</div>
+                    <div className={styles.dragText}>Drag & Drop</div>
+                </label>
 
+                {console.log("Selected Images Length: ", selectedImages.length)}
+                {console.log("Form Values Length: ", formValues[id].length)}
+
+            </div>
+
+            <div className={styles.footer}>
+                <label className={styles.uploadText} htmlFor="image-upload">
+                    Upload Images
+                </label>
+
+                {(selectedImages.length !== 0 || formValues[id].length !== 0) && 
+                    <button 
+                        className={styles.deleteText} 
+                        onClick={deleteImages}
+                        type="button"
+                    >
+                        Delete Images
+                    </button>
+                }
+            </div>
         </div>
-        </>
     );
 
 }
