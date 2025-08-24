26a5e8f01f3865e310e679195349d9bd0e9e8e88
Lavender Hwang <lhwang914@gmail.com>
2025-08-24 13:48:51 -0700

    Replace hex codes with root color vars

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,6 @@ function App() {
         <Route path="/landing" element={<Landing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-
         <Route path="/assessment" element={<Assessment />} />
       </Routes>
     </BrowserRouter>
